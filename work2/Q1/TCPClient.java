a5d56817b3ca2c3c7224a6511f0691dd077ab268
Beatriz Precebes <be.precebes@gmail.com>
2019-04-27 15:05:09 -0300

    Add files via upload

--- work2/Q1/TCPClient.java
+++ work2/Q1/TCPClient.java
@@ -1,1 +1,34 @@
-// A bea qe fez
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class TCPClient {
+	int serverPort;
+	Socket s;    
+	DataInputStream in;
+	DataOutputStream out; //= new DataOutputStream( s.getOutputStream());
+
+    public TCPClient(){
+		serverPort = 7896;
+		s = new Socket("localhost", serverPort);
+		in = new DataInputStream( s.getInputStream());
+		out = new DataOutputStream( s.getOutputStream());
+	}
+	 
+	public void sendRequest(String request){
+		out.writeUTF(request);
+	}
+
+	public String getResponse(){
+		String info = in.readUTF();
+		return info;
+	}
+
+	public void close(){
+		try{
+			s.close();
+		} catch (IOException e){
+			System.out.println("close: " + e.getMessage());
+		}
+	}
+}
---
+++ work2/Q1/User.java
@@ -0,0 +1,25 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class User{
+
+    public static void main(){
+        TCPClient userClient = new TCPClient();
+        String resp = new String();
+        String info = new String();
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Informe o que deseja calcular no seguinte formato:\n\t\"n1 op n2\"");
+        info = s.nextLine();
+
+        userClient.sendRequest(info);
+
+        resp = userClient.getResponse();
+
+        System.out.println(resp);
+        
+        s.close();
+        userClient.close();
+    }
+}
